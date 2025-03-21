6cbdc81d22cf1cd6671832dbce8c505cbd4897f0
Om Swami <reach.oms2004@gmail.com>
2025-03-22 02:23:58 +0530

    async web assembly
    Signed-off-by: Om Swami <reach.oms2004@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+module.exports = {
+  webpack: (config, { isServer }) => {
+    //enable asynchronous WebAssembly
+    config.experiments = {
+      asyncWebAssembly: true,
+      layers: true,
+    }
 
-export default nextConfig;
+    // fix warnings for async functions in the browser
+    if (!isServer) {
+      config.output.environment = {
+        ...config.output.environment,
+        asyncFunction: true,
+      }
+    }
+
+    return config
+  },
+} as NextConfig
